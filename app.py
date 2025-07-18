346c5ec1db99a54bb32d28a224e02a8c7070d53a
Naki <alicia.neko@mail.ru>
2025-07-18 13:09:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def ruta_personalizada(subpath):
 
 import os
 
-port = int(os.environ.get("PORT", 8000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT"))
+    app.run(host="0.0.0.0", port=port)
 
