24c280d283e648a9b803f8c7c074a05e28d3eb03
Naki <alicia.neko@mail.ru>
2025-07-18 13:18:28 -0400

    Add files via upload

--- sh.sh
+++ sh.sh
@@ -1,12 +1,4 @@
+mkdir server
+python3 -m http.server -d server &
+python3 app.py
 #!/bin/bash
-
-# Render asigna el puerto en $PORT
-PORT=${PORT:-8000}
-
-mkdir -p server
-
-# Servidor estático en otro puerto que NO sea $PORT
-python3 -m http.server 9999 -d server &
-
-# Flask escucha en el puerto asignado por Render
-exec python3 app.py
