8ec43d38c07fb3f46591b40011aeb4b7c215fb8a
Naki <alicia.neko@mail.ru>
2025-07-18 14:05:57 -0400

    Update app.py Y entoncez?

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
+from flask import request
+from scraper import obtener_galeria
+from scraper_2 import obtener_detalles
+import os
 app = Flask(__name__)
 
+web_1 = "https://nhentai.net"  # 🔁 Cambia por tu URL real
+
 @app.route("/")
-def index():
-    return "<h1>¡Render detecta esto! 🎯</h1>"
+def galeria():
+    resultado = obtener_galeria(web_1)
+    return render_template(
+        "galeria.html",
+        imagenes=resultado["imagenes"],
+        botones=resultado["botones"],
+        web_1=web_1
+    )
+
+@app.route("/g/<code>")
+def detalles(code):
+    resultado = obtener_detalles(web_1, code)
+    return render_template(
+        "detalles.html",
+        texto=resultado["texto"],
+        imagenes=resultado["imagenes"],
+        code=code,
+        web_1=web_1
+    )
+
+@app.route("/<path:subpath>")
+def ruta_personalizada(subpath):
+    full_url = f"{web_1}{request.full_path}"
+    resultado = obtener_galeria(full_url)
+    return render_template(
+        "galeria.html",
+        imagenes=resultado["imagenes"],
+        botones=resultado["botones"],
+        web_1=full_url
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
