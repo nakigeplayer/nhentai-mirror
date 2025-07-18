002fd797957f4692805539c589082fb1c0eb1f2c
Naki <alicia.neko@mail.ru>
2025-07-18 12:38:45 -0400

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Crear carpeta si no existe
-mkdir -p server
-
-# Iniciar servidor estático en segundo plano
-python3 -m http.server -d server --bind 0.0.0.0 --port 9999 &
-
-# Iniciar Flask como proceso principal
+mkdir server
+python3 -m http.server -d server &
 python3 app.py
