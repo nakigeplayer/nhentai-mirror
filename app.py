4d47c3db9c7d89e05a81ac08977cc2ccda70064b
Naki <alicia.neko@mail.ru>
2025-07-18 12:42:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from scraper import obtener_galeria
 from scraper_2 import obtener_detalles
 import os
-# Hola
 
 app = Flask(__name__)
 web_1 = "https://nhentai.net"  # 🔁 Cambia por tu URL real
-
-from flask import Flask, render_template
-from scraper import obtener_galeria
-from scraper_2 import obtener_detalles
 
 @app.route("/")
 def galeria():
@@ -32,8 +27,6 @@ def detalles(code):
         web_1=web_1
     )
 
-from flask import request
-
 @app.route("/<path:subpath>")
 def ruta_personalizada(subpath):
     full_url = f"{web_1}{request.full_path}"
@@ -44,8 +37,7 @@ def ruta_personalizada(subpath):
         botones=resultado["botones"],
         web_1=full_url
     )
-    
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
