a20387af183db1be328a0e0f567cd18707861bd7
Naki <alicia.neko@mail.ru>
2025-07-21 13:54:43 -0400

    Update tab.js

--- static/js/tab.js
+++ static/js/tab.js
@@ -1,1 +1,15 @@
+
 console.log("JS externo cargado correctamente.");
+function cargarLocal(url, id) {
+    fetch("/local-img?url=" + encodeURIComponent(url))
+        .then(res => res.text())
+        .then(base64 => {
+            console.log("Base64 recibido:", base64.slice(0, 100)); // muestra un fragmento
+            if (base64 && base64.startsWith("data:image")) {
+                document.getElementById(id).src = base64;
+            } else {
+                console.warn("No se recibió imagen válida");
+            }
+        })
+        .catch(err => console.error("Error en carga local:", err));
+}
